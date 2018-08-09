f4f9150a6025984a1430b7e2d8883022fa9cc919
Milan Milinkovic <milan@spectral.energy>
2018-08-09 23:56:14 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# Etihad_Analytics_Test" 
+## Etihad Analytics Test
+
+This repository contains the code utilized for solving given analytics task. 
+
+The analysis was carried out in Python 3 programming language. All utilized modules were listed in 'requirements.txt' file, and they can easily be installed with 'pip install requirements.txt' command.
+
+For coding simplicity and maintability, the code was structured in a object-oriented style. All major code parts (data parsing, downloading, analysis and visualization) were coded into separate classes (found in folder 'src/'), which allowed the implementation of a super simple final script.
+
+The obtained analysis results can be replicated by running the 'main.py' script (or 'main.ipynb' Jupyter notebook).
--- main.py
+++ main.py
@@ -9,19 +9,29 @@ parser = Parser()
 analyzer = Analyzer()
 plotter = Plotter(fig_size=(10, 5))
 
-# Parse the data (read, fill and process):
-# If previously parsed, just load the data:
-if os.path.exists('updated_meteo_data.xlsx'):
+# Parse the data:
+
+# If already parsed - just load the saved data:
+if os.path.exists('data/updated_meteo_data.xlsx'):
+    # This method loads previously parsed data
     dfs = parser.load_parsed_dfs()
-# Else parse the data from scratch:
+# If not - parse the data and save to file:
 else:
-    dfs = parser.parse_data()
+    # This method reads original data, fills missing values,
+    # performs required processing and saves obtained data
+    dfs = parser.parse_data(save=True)
 
 # Perform comfort level analysis and save counts to file:
+
+# This method computes parameter-specific comfort indicators,
+# then uses them to calculate daily comfort levels,
+# and performs a daily comfort count over all cities
 count = analyzer.calc_day_counts(dfs)
 print(count.head())
 
 # Plot required graphs:
+
+# These methods output required violin graphs
 plotter.plot_temp_graph(dfs)
 plotter.plot_humidity_graph(dfs)
 plotter.plot_sunlight_graph(dfs)
